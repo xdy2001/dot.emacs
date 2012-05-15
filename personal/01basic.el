407f8cf5e38469862af13e7288c8e88763b62f65
ehonlia <hongxin.liang@ericsson.com>
2012-05-15 11:27:56 +0800

    switch to scratch buffer

--- personal/01basic.el
+++ personal/01basic.el
@@ -380,4 +380,19 @@ that was stored with ska-point-to-register."
 (global-set-key (kbd "C-e") 'end-of-line)
 
 ;; Empty initial scratch buffer
-(setq initial-scratch-message nil)
+(setq initial-scratch-message "")
+
+;; Switch to *scratch* buffer quickly
+(defun switch-buffer-scratch ()
+  "Switch to the scratch buffer. If the buffer doesn't exist,
+create it and write the initial message into it."
+  (interactive)
+  (let* ((scratch-buffer-name "*scratch*")
+         (scratch-buffer (get-buffer scratch-buffer-name)))
+    (unless scratch-buffer
+      (setq scratch-buffer (get-buffer-create scratch-buffer-name))
+      (with-current-buffer scratch-buffer
+        (lisp-interaction-mode)
+        (insert initial-scratch-message)))
+    (switch-to-buffer scratch-buffer)))
+(global-set-key (kbd "C-c s") 'switch-buffer-scratch)
