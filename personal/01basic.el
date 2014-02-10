72175ac0b5bf37e4eee99e2912eb9962de6eea91
Honnix Liang <hxliang1982@gmail.com>
2014-02-10 21:22:40 +0100

    switch to melpa

--- personal/01basic.el
+++ personal/01basic.el
@@ -27,7 +27,7 @@
 
 (setq package-archives '(("ELPA" . "http://tromey.com/elpa/") 
                           ("gnu" . "http://elpa.gnu.org/packages/")
-                          ("marmalade" . "http://marmalade-repo.org/packages/")))
+                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 
 ;; Set font
